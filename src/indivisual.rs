e8ce2f5a4a1a23b110a3f7dccfafeaad25c80d0e
ikanago <28985004+ikanago@users.noreply.github.com>
2020-03-31 15:32:51 +0900

    ✨ Implement main iteration of genetic process.

--- src/indivisual.rs
+++ src/indivisual.rs
@@ -1,20 +1,45 @@
 use rand::Rng;
+use std::mem;
+
+type Gene = Vec<u8>;
 
 #[derive(Clone, Debug)]
 pub struct Indivisual {
-    pub gene: Vec<u8>,
-    pub evaluation: f64,
+    pub gene: Gene,
+    pub evaluation: usize,
 }
 
 impl Indivisual {
     pub fn new(gene_length: usize) -> Self {
         let mut rng = rand::thread_rng();
-        let gene: Vec<u8> = (0..gene_length).map(|_| {
-            rng.gen_range(0, 2)
-        }).collect();
+        let gene: Gene = (0..gene_length).map(|_| rng.gen_range(0, 2)).collect();
         Self {
             gene,
-            evaluation: 0_f64,
+            evaluation: 0,
         }
+    }
+
+    pub fn evaluate(&mut self) -> &mut Self {
+        self.evaluation = self.gene.iter().sum::<u8>() as usize;
+        self
+    }
+
+    pub fn build(&self) -> Self {
+        Self {
+            gene: self.gene.clone(),
+            evaluation: self.evaluation,
+        }
+    }
+
+    pub fn cross_over(parent1: &mut Indivisual, parent2: &mut Indivisual, gene_length: usize) {
+        let mut rng = rand::thread_rng();
+        let split_point1 = rng.gen_range(1, gene_length - 1);
+        let split_point2 = rng.gen_range(split_point1 + 1, gene_length);
+
+        for i in split_point1..=split_point2 {
+            mem::swap(&mut parent1.gene[i], &mut parent2.gene[i]);
+        }
+        parent1.evaluate();
+        parent2.evaluate();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,69 @@
+use std::fmt;
+
 mod indivisual;
 use indivisual::Indivisual;
 
 fn main() {
-    let simulator = Simulator::new(10, 10, 0_f64);
-    println!("{:?}", simulator);
+    let mut simulator = Simulator::new(10, 20, 0_f64);
+    simulator.run(10);
 }
 
 #[derive(Clone, Debug)]
 struct Simulator {
+    gene_length: usize,
     current_generation: usize,
     mutate_rate: f64,
     indivisuals: Vec<Indivisual>,
 }
 
 impl Simulator {
-    fn new(num_indivisuals: usize, gene_length: usize, mutate_rate: f64,) -> Self {
-        let indivisuals: Vec<Indivisual> = (0..num_indivisuals).map(|_| {
-            Indivisual::new(gene_length)
-        }).collect();
+    fn new(num_indivisuals: usize, gene_length: usize, mutate_rate: f64) -> Self {
+        let mut indivisuals: Vec<Indivisual> = (0..num_indivisuals)
+            .map(|_| Indivisual::new(gene_length).evaluate().build())
+            .collect();
+        Self::sort_by_evaluation(&mut indivisuals);
         Self {
+            gene_length,
             current_generation: 1,
             mutate_rate,
-            indivisuals
+            indivisuals,
         }
+    }
+
+    fn run(&mut self, iteration_count: usize) {
+        Self::sort_by_evaluation(&mut self.indivisuals);
+        println!("{}", self);
+        for _ in 0..iteration_count {
+            self.proceed_generation();
+            self.current_generation += 1;
+            println!("{}", self);
+        }
+    }
+
+    fn proceed_generation(&mut self) {
+        let mut offspring: Vec<Indivisual> = Vec::with_capacity(self.indivisuals.len());
+        for _ in 0..self.indivisuals.len() / 2 {
+            let (mut parent1, mut parent2) = self.select();
+            Indivisual::cross_over(&mut parent1, &mut parent2, self.gene_length);
+            offspring.push(parent1);
+            offspring.push(parent2);
+        }
+        self.indivisuals = offspring;
+        Self::sort_by_evaluation(&mut self.indivisuals);
+    }
+
+    fn select(&mut self) -> (Indivisual, Indivisual) {
+        (self.indivisuals[0].clone(), self.indivisuals[1].clone())
+    }
+
+    fn sort_by_evaluation(indivisuals: &mut Vec<Indivisual>) {
+        indivisuals.sort_by(|x, y| (y.evaluation).cmp(&x.evaluation));
+    }
+}
+
+impl fmt::Display for Simulator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write! {f, "--------------------Generation {}--------------------\ngene_length: {}\nmutate_rate: {}\nmost elite indivisuals: {:?}"
+        , self.current_generation, self.gene_length, self.mutate_rate, self.indivisuals[0]}
     }
 }
